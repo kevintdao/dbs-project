82149c28181fb264b576fcd773005976d0b4a1f4
MGLeyda <79680573+MGLeyda@users.noreply.github.com>
2022-04-06 15:29:53 -0500

    Merge branch 'main' of https://github.com/kevintdao/dbs-project

--- queries/queries.js
+++ queries/queries.js
@@ -1,7 +1,7 @@
 import { executeQuery } from '../config/db'
 
 const getData = async (req, res) => {
-  let data = await executeQuery("select * from city", [])
+  let data = await executeQuery("select * from genre", [])
   res.send(data)
 }
 export { getData }
