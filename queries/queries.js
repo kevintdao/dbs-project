1e50228716206901d9e65522c05a6ca8a85d78f5
Kevin Dao <kevin-dao@uiowa.edu>
2022-04-06 15:40:16 -0500

    Add SQL for inserting into User table

--- queries/queries.js
+++ queries/queries.js
@@ -1,7 +1,13 @@
 import { executeQuery } from '../config/db'
 
 const getData = async (req, res) => {
-  let data = await executeQuery("select * from city", [])
+  let data = await executeQuery("select * from genre", [])
   res.send(data)
 }
-export { getData }
+
+const insertUser = async (req, res) => {
+  let data = await executeQuery("INSERT INTO USER(name, age) VALUES(?,?)", ['test', 18])
+  res.send(data)
+}
+
+export { getData, insertUser }
