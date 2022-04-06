43bbbe065cbf415fd5c34364e930e961b852c0b8
Kevin Dao <kevin-dao@uiowa.edu>
2022-04-06 15:54:46 -0500

    Fix tailwind installation

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
 export default function Home() {
+  const styles = {
+    input: "p-2 border border-gray-300 rounded"
+  }
+
   return (
-    <div>
-      Hello World
+    <div className='space-y-4 mt-2'>
+      <h2 className='font-bold'>Homepage</h2>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" className={styles.input} />
+      </div>
+      <div>
+        <label htmlFor="age">Age</label>
+        <input type="number" name="age" id="age" className={styles.input} />
+      </div>
     </div>
   )
 }
