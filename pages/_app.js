d8464513f7bfc6cf68143dd68f99e5b6ace11614
Kevin Dao <kevin-dao@uiowa.edu>
2022-04-12 22:29:29 -0500

    Update homepage styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className='max-w-5xl px-2 container mx-auto pb-3'>
+      <Component {...pageProps} />
+    </div>
+  )
+  
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,33 @@ import React from 'react'
 
 export default function Home() {
   const styles = {
-    input: "p-2 border border-gray-300 rounded"
+    form: "space-y-4 mt-2", 
+    input_container: "flex flex-col",
+    input: "p-2 border border-gray-300 rounded",
+    button: "bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    console.log(e.target.name.value)
   }
 
   return (
     <div className='space-y-4 mt-2'>
       <h2 className='font-bold'>Homepage</h2>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" className={styles.input} />
-      </div>
-      <div>
-        <label htmlFor="age">Age</label>
-        <input type="number" name="age" id="age" className={styles.input} />
-      </div>
+      <form onSubmit={onSubmit} className={styles.form}>
+        <div className={styles.input_container}>
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" id="name" className={styles.input} required/>
+        </div>
+
+        <div className={styles.input_container}>
+          <label htmlFor="age">Age</label>
+          <input type="number" name="age" id="age" className={styles.input} required/>
+        </div>
+
+        <input className={styles.button} type="submit" value="Submit" />
+      </form>
     </div>
   )
 }
