116c286026981682a11af8fc7f92f9d0d41a2361
Kevin Dao <kevin-dao@uiowa.edu>
2022-04-28 22:07:49 -0500

    Add radio button to display only table or graph

--- pages/results.js
+++ pages/results.js
@@ -27,6 +27,12 @@ ChartJS.register(
 
 export default function results() {
   const [data, setData] = useState()
+  const [userChoice, setUserChoice] = useState('table')
+  const [allChoice, setAllChoice] = useState('table')
+
+  const styles = {
+    radio_button: 'form-radio h-4 w-4 border border-gray-300 focus:outline-none transition duration-200 mt-1 align-top bg-center bg-contain mr-2 cursor-pointer'
+  }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -58,16 +64,36 @@ export default function results() {
       <Header title='Video Games Results' />
       <div>
         <h4 className='font-bold text-xl'>User Results</h4>
-        <Results data={data.result} />
-        <ResultsGraph data={data.percent} />
+        <div className='space-x-4'>
+          <label htmlFor="u-table">
+            <input className={styles.radio_button} type="radio" name="u-table" id="u-table" checked={userChoice == 'table'} onChange={() => setUserChoice('table')} />
+            Table
+          </label>
+          <label htmlFor="u-graph">
+            <input className={styles.radio_button} type="radio" name="u-graph" id="u-graph" checked={userChoice == 'graph'} onChange={() => setUserChoice('graph')} />
+            Graph
+          </label>
+        </div>
+        {userChoice == 'table' && <Results data={data.result} />}
+        {userChoice == 'graph' && <ResultsGraph data={data.percent} />}
       </div>
 
       <hr />
 
       <div>
         <h4 className='font-bold text-xl'>All Results</h4>
-        <AllResults data={data.all} />
-        <AllResultsGraph data={data.all} />
+        <div className='space-x-4'>
+          <label htmlFor="a-table">
+            <input className={styles.radio_button} type="radio" name="a-table" id="a-table" checked={allChoice == 'table'} onChange={() => setAllChoice('table')} />
+            Table
+          </label>
+          <label htmlFor="a-graph">
+            <input className={styles.radio_button} type="radio" name="a-graph" id="a-graph" checked={allChoice == 'graph'} onChange={() => setAllChoice('graph')} />
+            Graph
+          </label>
+        </div>
+        {allChoice == 'table' && <AllResults data={data.all} />}
+        {allChoice == 'graph' && <AllResultsGraph data={data.all} />}
       </div>
     </div>
   )
